0526b1803d6374096acded0d6a7c2cc00f1b2c31
Ubuntu <ubuntu@ip-10-0-2-152.us-west-1.compute.internal>
2014-03-04 00:06:12 +0000

    docklet production test

--- test/docklet.js
+++ test/docklet.js
@@ -1,67 +1,109 @@
 var request = require('supertest');
 var Docker = require('dockerode');
-var host = 'localhost';
-var dockletUrl = function (path) {
-  path = path || '';
-  var domain = 'http://'+host+':4244';
-  return domain+path;
-};
-var dockerUrl = function (path) {
-  path = path || '';
-  var domain = 'http://'+host+':4243';
-  return domain+path;
-};
-var docker = new Docker(dockerUrl());
+var host = 'http://localhost';
+var docklet = request(host+':4244');
+var dockworker, dockworkerUrl;
+var docker = new Docker({
+  host: host,
+  port: 4243
+});
+var uuid = require('uuid');
+var ShoeClient = require('./lib/ShoeClient');
+var MuxDemux = require('mux-demux');
 
-var ip = require("os").networkInterfaces()[configs.networkInterface].filter(function(iface) {
+var ip = require("os").networkInterfaces().eth0.filter(function(iface) {
   return iface.family === "IPv4";
 })[0].address;
 
 var container;
+function toEnv (obj) {
+  return Object.keys(obj).reduce(function (env, key) {
+    env.push(key+'='+obj[key]);
+    return env;
+  }, []);
+}
 
-describe('dockletUrl', function () {
+var serviceToken = 'services-' + uuid.v4();
+describe('docklet', function () {
   before(function () {
-    this.repo = 'registry.runnable.com/UvKWVfRki308AAAh';
+    this.repo = 'registry.runnable.com/runnable/UvKWVfRki308AAAh';
   });
 
   it('should find a container', function (done) {
-    request.post(dockletUrl('/find'))
+    docklet.post('/find')
       .send({ repo: this.repo })
-      .expect(200, function (res) {
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
         res.body.should.equal(ip);
-      })
-      .end(done);
+        done();
+      });
   });
-  var createBody = { Image: 'base',
-    Volumes: { '/dockworker': {} },
-    PortSpecs: [ '80', '15000' ],
-    Cmd: [ 'node', '/dockworker' ],
-    Env: [ 'STOP_URL=http://localhost:4243/containers/services-f5d237ac-f2bd-4848-a1f0-655713e1b889/stop' ]
-  };
   it('should create a container', function (done) {
-    var body = createBody;
-
+    var self = this;
+    var body =  {
+      Image: this.repo,
+      Volumes: { '/dockworker': {} },
+      PortSpecs: [ '80', '15000' ],
+      Cmd: [ '/dockworker/bin/node', '/dockworker' ],
+      Env: toEnv({
+        SERVICES_TOKEN: serviceToken,
+        RUNNABLE_START_CMD: 'npm start'
+      })
+    };
     docker.createContainer(body, function (err, body) {
       if (err) return done(err);
       container = body;
+      done();
     });
   });
   describe('running container', function () {
-    it('should start a container', function (done) {
-      container.start(done);
+    it('should start', function (done) {
+      this.timeout(10*1000);
+      container.start({
+        Binds: ["/home/ubuntu/dockworker:/dockworker:ro"]
+      }, done);
     });
-  });
-  it('should find a containers ports', function () {
-
-  });
-
-
-  describe('dockworker', function () {
-    it('should get the containers service token', function () {
-
+    it('should find its ports', function (done) {
+      container.inspect(function (err, data) {
+        if (err) return done(err);
+        var port = data.NetworkSettings.PortMapping.Tcp[15000];
+        port.should.be.type('string');
+        dockworkerUrl = host+':'+port;
+        dockworker = request(dockworkerUrl);
+        done();
+      });
     });
-    it('should get connect to the terminal and echo back', function () {
-
+    // dockworker
+    it('should get the containers service token', function (done) {
+      var self = this;
+      dockworker.get('/api/servicesToken')
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err);
+          res.text.should.equal(serviceToken)
+          done();
+        });
+    });
+    it('should run echo', function (done) {
+      var socket = 'ws://'+dockworkerUrl.split('//')[1];
+      var stream = new ShoeClient(socket+'/streams/terminal');
+      var muxDemux = new MuxDemux(onStream);
+      stream.pipe(muxDemux).pipe(stream);
+      function onStream(stream) {
+        if (stream.meta === 'terminal') {
+          onTerminal(stream);
+        }
+      }
+      function onTerminal(stream) {
+        var count = 0;
+        stream.on('data', function (data) {
+          if (/npm start\r\n/.test(data)) {
+            done();
+          }
+        });
+        stream.write('echo $RUNNABLE_START_CMD\n');
+      }
     });
   });
 });
