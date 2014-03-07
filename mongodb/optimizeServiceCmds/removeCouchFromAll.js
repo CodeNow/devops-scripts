9e29fd66c1374d18733ccac2c15b2621692de537
tjmehta <tj@init.me>
2014-03-06 19:44:26 -0800

    remove couch log

--- mongodb/optimizeServiceCmds/removeCouchFromAll.js
+++ mongodb/optimizeServiceCmds/removeCouchFromAll.js
@@ -6,7 +6,7 @@ var fields = {
 };
 db.images.find(query, fields).forEach(function (image) {
   var new_service_cmds = image.service_cmds.replace('couchdb;', '').trim();
-  print(['update',image._id,new_service_cmds].join(' '));
+  print(['remove couch',image._id,new_service_cmds].join(' '));
   db.images.update({ _id: image._id }, {
     $set: {
       service_cmds: new_service_cmds
