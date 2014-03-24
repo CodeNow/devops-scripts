ec5cbce6d09771f5395e43d99c1203235e8d593f
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-03-24 13:25:40 -0700

    make NODE_ENV input

--- docks/startDockletAndBouncer.sh
+++ docks/startDockletAndBouncer.sh
@@ -1,4 +1,8 @@
+if [[ $# -ne 1 ]]; then
+	echo "usage ./startDockletAndBouncer.sh <NODE_ENV>"
+	echo "where NODE_ENV is one of the following production, staging, integration, development"
+fi
 sudo pm2 kill
-sudo NODE_ENV=production pm2 start docklet/lib/bouncer.js -n bouncer -i 5
-sudo NODE_ENV=production pm2 start docklet/lib/index.js -n docklet
+sudo NODE_ENV=$1 pm2 start docklet/lib/bouncer.js -n bouncer -i 5
+sudo NODE_ENV=$1 pm2 start docklet/lib/index.js -n docklet
 sudo pm2 logs
