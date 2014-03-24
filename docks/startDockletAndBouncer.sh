a688a42e11154e10049c35728462a670e583555d
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-03-24 16:05:03 -0700

    remove start log after finish startDockelet script

--- docks/startDockletAndBouncer.sh
+++ docks/startDockletAndBouncer.sh
@@ -5,4 +5,4 @@ fi
 sudo pm2 kill
 sudo NODE_ENV=$1 pm2 start docklet/lib/bouncer.js -n bouncer -i 5
 sudo NODE_ENV=$1 pm2 start docklet/lib/index.js -n docklet
-sudo pm2 logs
+echo "bouncer and docklet started"
