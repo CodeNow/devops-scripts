3ecff9c3493b230a5cd549ccb1aad76359289946
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-04-02 11:18:44 -0700

    update to use nu bouncer

--- docks/startDockletAndBouncer.sh
+++ docks/startDockletAndBouncer.sh
@@ -3,6 +3,6 @@ if [[ $# -ne 1 ]]; then
 	echo "where NODE_ENV is one of the following production, staging, integration, development"
 fi
 sudo pm2 kill
-sudo NODE_ENV=$1 pm2 start docklet/lib/bouncer.js -n bouncer -i 5
+sudo NODE_ENV=$1 pm2 start docklet/bouncer/index.js -n bouncer -i 5
 sudo NODE_ENV=$1 pm2 start docklet/lib/index.js -n docklet
 echo "bouncer and docklet started"
