8d2d55ed39b1ff7d36516e1e70e1cac466599ffd
Ubuntu <ubuntu@ip-10-0-2-217.us-west-1.compute.internal>
2014-04-16 23:28:47 +0000

    fix image names from hex64. add new image file

--- docks/pullTopImages/getAllImageUrls.js
+++ docks/pullTopImages/getAllImageUrls.js
@@ -3,36 +3,41 @@ var mongo = require('mongoskin');
 var hex64 = require('hex64');
 var async = require('async');
 var Docker = require('dockerode');
-var docker = new Docker({socketPath: '/var/run/docker.sock'});
+var docker = new Docker({
+    host: "http://localhost",
+    port: 4242
+  });
 var fs = require('fs');
 var path = require('path');
-var limit = 2000;
+var limit = 1500;
 function pullTopImagesFromDb () {
-  var db = mongo.db("mongodb://localhost:27017/runnable2", {native_parser:true});
+  var db = mongo.db("mongodb://10.0.1.47:27017/runnable2", {native_parser:true});
   db.bind('images');
   db.images.find({}, { _id: 1, revisions:1 })
     .sort({ views: -1 })
     .limit(limit)
-    .toArray(mapAndCreateFile);
+    .toArray(mapAndCreateFile)
+    .close();
 }
 function mapAndCreateFile (err, images) {
   if (err) throw err;
 
   var imageUrls = images.map(function (image) {
-    var imageId64;
+    var repoName;
     if (image.revisions && image.revisions.length) {
       var revision = image.revisions[image.revisions.length-1];
       var repoId = (revision.repo || revision._id).toString();
-      imageId64 = hex64.transform(repoId);
-    }
-    else {
-      imageId64 = hex64.transform(image._id.toString());
+      repoName = repoId
+    } else {
+      repoName = image._id.toString();
     }
 
-    return 'registry.runnable.com/runnable/'+imageId64;
+    return 'registry.runnable.com/runnable/'+repoName;
   });
-
   var filename = 'top'+limit+'Images'+Date.now()+'.js';
+  console.log("writing to file" + filename);
   fs.writeFileSync(path.join(__dirname, filename), 'module.exports='+JSON.stringify(imageUrls));
+  console.log("done ctrl^c to close");
 }
+console.log("start");
 pullTopImagesFromDb();
