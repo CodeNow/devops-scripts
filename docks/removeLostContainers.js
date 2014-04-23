665e02a0ffa207ab943c2969f727c557625c7205
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-04-22 23:30:41 -0700

    update container scripts

--- docks/removeLostContainers.js
+++ docks/removeLostContainers.js
@@ -1,20 +1,27 @@
 // remove orphen containers
 var tokens = require('../mongodb/getContainerId.js');
-var redis = require('redis').createClient("6379", "10.0.1.243");
+var redis = require('redis').createClient("6379", "localhost");
 tokens.getServicetokens(gotTokens);
 
+var count = 0;
+var tcount = 0;
 function gotTokens(err, tokens) {
+  tcount = tokens.length;
   for (var i = 0; i < tokens.length; i++) {
     redis.hgetall('harbourmasterSession:' + tokens[i].servicesToken, gotSession);
   }
 }
 
 function gotSession (err, data) {
+  count++;
   if (err) {
     // console.error("error: " +err);
   } else if (data === null) {
     // console.error('error: Session not found ' + err);
   } else {
     console.log(data.containerId);
+  }
+  if(count === tcount) {
+    redis.quit();
   }
 }
