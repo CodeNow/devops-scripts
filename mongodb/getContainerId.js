32e8601985dc8545ef829b3eb72641c820471df7
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-04-22 23:24:20 -0700

    update container scripts

--- mongodb/getContainerId.js
+++ mongodb/getContainerId.js
@@ -1,14 +1,21 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://10.0.1.176:27017/runnable2', function(err, db) {
-  if(err) throw err;
-  console.log("connected");
-  var collection = db.collection('containers');
-
-  // Locate all the entries using find
-  collection.find({},{_id:0,servicesToken:1}).toArray(function(err, results) {
-    console.dir(results);
-    // Let's close the db
-    db.close();
+var getServicetokens = function(cb) { 
+  MongoClient.connect('mongodb://10.0.1.47:27017/runnable2', function(err, db) {
+    if(err) {
+      console.error("err"+err);
+      cb(err);
+      return;
+    }
+    var collection = db.collection('containers');
+    // Locate all the entries using find
+    collection.find({},{_id:0,servicesToken:1}).toArray(function(err, results) {
+      // console.dir(results);
+      // Let's close the db
+      db.close();
+      cb(err, results);
+    });
   });
-});
+};
+
+module.exports.getServicetokens = getServicetokens;
