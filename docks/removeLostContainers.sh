eb66f1979fdbb2506026fc0aac9a4c48672bbe1f
Ubuntu <ubuntu@ip-10-0-2-203.us-west-1.compute.internal>
2014-04-23 19:20:35 +0000

    update removeLostContainers script to add more output

--- docks/removeLostContainers.sh
+++ docks/removeLostContainers.sh
@@ -1,6 +1,18 @@
-for CONT in `ls /var/lib/docker/container`; do
+TOTAL_NUM_CONT=$(ls /var/lib/docker/containers | wc -l)
+CUR_CNT=0
+NUM_RM=0
+for CONT in `ls /var/lib/docker/containers`; do
+  CUR_CNT=$((CUR_CNT+1))
+  echo "processing $CUR_CNT out of $TOTAL_NUM_CONT"
   COUNT=$(grep $CONT ./goodContaienrs | wc -l)
   if [[ "$COUNT" -eq "0" ]]; then
-    echo "docker rm $CONT"
+    NUM_RM=$((NUM_RM+1))
+    docker rm $CONT
   fi
 done
+
+echo "COMPLETE STATUS"
+echo ""
+echo "num containers processed $TOTAL_NUM_CONT"
+echo "num containers removed $NUM_RM"
+echo "num containers kept " $((TOTAL_NUM_CONT-NUM_RM))
