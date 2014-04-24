89fa2002f7622ae58ccdf0f4c050d6b6d1b7af44
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-04-23 19:06:58 -0700

    adding error checks for deleteUnusedTokens.js

--- Harbour/deleteUnusedTokens.js
+++ Harbour/deleteUnusedTokens.js
@@ -1,7 +1,7 @@
 // remove service tokens which cant be found in mongo. 
 var tokens = require('../mongodb/getContainerId.js');
 var redis = require('redis').createClient("6379", "localhost");
-var mongoHost = 'mongodb://10.0.1.176:27017/runnable2';
+var mongoHost = 'mongodb://10.0.1.34:27017/runnable2';
 var mongoServicesTokens = [];
 
 tokens.getServicesToken(mongoHost, gotServicesToken);
@@ -11,7 +11,7 @@ function gotServicesToken(err, servicesTokens) {
     console.dir(err);
     return;
   }
-  console.log("got servicesTokens from mongodb");
+  console.log("got servicesTokens from mongodb. num: "+servicesTokens.length);
   mongoServicesTokens = servicesTokens;
   redis.keys("harbourmasterSession:*", gotKeys);
 }
@@ -21,14 +21,27 @@ function gotKeys(err, keys) {
     console.dir(err);
     return;
   }
-  console.log("got keys from redis");
+  console.log("got keys from redis. num: "+keys.length);
+  var numToRemove = keys.length - mongoServicesTokens.length;
+  if (numToRemove <= 0) {
+    console.error("more keys in mongo then redis");
+    return;
+  }
+  console.log("we need to remove "+numToRemove);
   var multi = redis.multi();
+  var numRemoving = 0;
   keys.forEach(function (key, i) {
-    if (mongoServicesTokens.indexOf(key) <= -1) {
+    token = key.slice(21,-1);
+    if (mongoServicesTokens.indexOf(token) <= -1) {
         console.log("deleting: "+key);
+        numRemoving++;
         multi.del(key);
     }
   });
+  if (numToRemove !== numRemoving) {
+    console.error("removing wrong number. removing: "+numRemoving+" should be removing: "+numToRemove);
+    return;
+  }
   multi.exec(function (err, replies) {
     if(err) {
       console.dir(err);
