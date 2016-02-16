9233656267075fd6efac21354fdc3c1267c3b7c2
Christopher M. Neill <chris@runnable.com>
2016-02-16 15:46:38 -0800

    Copy instead of link, change shell script modes

--- ansible/roles/container_start/files/findTagRunning.sh
+++ ansible/roles/container_start/files/findTagRunning.sh
@@ -1,1 +1,19 @@
-../container_kill_start/files/findTagRunning.sh
+#!/usr/bin/env bash
+
+IMAGE_NAME="$1"
+RUNNING_CONTAINERS=""
+
+CONTAINERS=`docker ps | grep -v '^CONTAINER' | awk '{print $1}'`
+
+if [ "" = "${CONTAINERS}" ] ; then
+    exit 0
+else
+    for container in ${CONTAINERS} ; do
+        docker inspect ${container} | grep -q ${IMAGE_NAME}
+        if [ ${?} -eq 0 ] ; then
+            RUNNING_CONTAINERS="${container} "
+        fi
+    done
+fi
+
+echo "${RUNNING_CONTAINERS}"
