0ac4fd5aa373d182394735fdfbdd110527a1bb7d
Christopher M. Neill <chris@runnable.com>
2016-04-22 16:30:17 -0700

    Normalizing variables and comments

--- ansible/roles/loggly/files/archiveOldLogs.sh
+++ ansible/roles/loggly/files/archiveOldLogs.sh
@@ -4,24 +4,28 @@
 # This is to be run only for legacy logs in /var/log, not application logs in /var/log/runnable
 #
 
+# we only want this run as root
 if [ "root" != `whoami` ] ; then
     echo "It don't mean a thing if it ain't got that swing."
     exit 127
 fi
 
-LOGDIR=/var/log
-ARCHDIR=/docker/archive
-DATE=`date +%Y%m%d%H%m`
+# legacy log path
+logdir=/var/log
+# store log archives here, purge manually
+archdir=/docker/archive
+datetime=`date +%Y%m%d%H%m`
 
-# these haven't been modified in > 24 hours, so "doing it live" should be OK:
+# these logfiles haven't been modified in > 24 hours, so moving them without cleanup up filehandles first should be OK:
 echo "Compressing logs > 24h"
-find ${LOGDIR} -maxdepth 2 -type f -mmin +1440 -name '*.log' -exec bzip2 -9 {} \; -print
+find ${logdir} -maxdepth 2 -type f -mmin +1440 -name '*.log' -exec bzip2 -9 {} \; -print
 echo "Restarting rsyslogd"
+# but we do need to clean the filehandles after, just in case
 service rsyslog restart
 
 # archive anything > 1wk
 echo "Archiving logs > 1wk"
-mkdir -p ${ARCHDIR} 2>&1
-find ${LOGDIR} -maxdepth 2 -type f -mtime +6 -name '*z' | xargs tar jcvpf ${ARCHDIR}/log-archive-${DATE}.tbz 
+mkdir -p ${archdir} 2>&1
+find ${logdir} -maxdepth 2 -type f -mtime +6 -name '*z' | xargs tar jcvpf ${archdir}/log-archive-${datetime}.tbz 
 echo "Purging logs > 1wk"
-find ${LOGDIR} -maxdepth 2 -type f -mtime +6 -exec rm -f {} \; -print
+find ${logdir} -maxdepth 2 -type f -mtime +6 -exec rm -f {} \; -print
--- ansible/roles/loggly/files/purgeLogs.sh
+++ ansible/roles/loggly/files/purgeLogs.sh
@@ -5,10 +5,10 @@
 # Runs from crontab.
 #
 
-LOGDIR=${1}
+logdir=${1}
 
 # We can compress anything older than 6 hours
-find ${LOGDIR} -mindepth 2 -type f -mmin +360 -name '*.log' -exec bzip2 -9 {} \; 
+find ${logdir} -mindepth 2 -type f -mmin +360 -name '*.log' -exec bzip2 -9 {} \; 
 
-# purge anything > 1wk
-find ${LOGDIR} -maxdepth 2 -type f -mtime +6 -exec rm -f {} \; 
+# We automatically purge anything > 1wk
+find ${logdir} -maxdepth 2 -type f -mtime +7 -exec rm -f {} \; 
