4e4823b156ff23071c0fec1c5c4dfea5655c8ba5
Christopher M. Neill <chris@runnable.com>
2016-04-25 17:59:56 -0700

    double-quote variables

--- ansible/roles/loggly/files/archiveOldLogs.sh
+++ ansible/roles/loggly/files/archiveOldLogs.sh
@@ -18,14 +18,14 @@ datetime=`date +%Y%m%d%H%m`
 
 # these logfiles haven't been modified in > 24 hours, so moving them without cleanup up filehandles first should be OK:
 echo "Compressing logs > 24h"
-find ${logdir} -maxdepth 2 -type f -mmin +1440 -name '*.log' -exec bzip2 -9 {} \; -print
+find "${logdir}" -maxdepth 2 -type f -mmin +1440 -name '*.log' -exec bzip2 -9 {} \; -print
 echo "Restarting rsyslogd"
 # but we do need to clean the filehandles after, just in case
 service rsyslog restart
 
 # archive anything > 6h
 echo "Archiving logs > 6h"
-mkdir -p ${archdir} 2>&1
-find ${logdir} -maxdepth 2 -type f -mtime +6 -name '*z' | xargs tar jcvpf ${archdir}/log-archive-${datetime}.tbz 
+mkdir -p "${archdir}" 2>&1
+find "${logdir}" -maxdepth 2 -type f -mtime +6 -name '*z' | xargs tar jcvpf "${archdir}"/log-archive-"${datetime}".tbz
 echo "Purging logs > 1wk"
-find ${logdir} -maxdepth 2 -type f -mtime +6 -exec rm -f {} \; -print
+find "${logdir}" -maxdepth 2 -type f -mtime +6 -exec rm -f {} \; -print
--- ansible/roles/loggly/files/purgeLogs.sh
+++ ansible/roles/loggly/files/purgeLogs.sh
@@ -5,10 +5,10 @@
 # Runs from crontab.
 #
 
-logdir=${1}
+logdir="${1}"
 
 # We can compress anything older than 6 hours
-find ${logdir} -mindepth 2 -type f -mmin +360 -name '*.log' -exec bzip2 -9 {} \; 
+find "${logdir}" -mindepth 2 -type f -mmin +360 -name '*.log' -exec bzip2 -9 {} \; 
 
 # We automatically purge anything > 1wk
-find ${logdir} -maxdepth 2 -type f -mtime +7 -exec rm -f {} \; 
+find "${logdir}" -maxdepth 2 -type f -mtime +7 -exec rm -f {} \; 
