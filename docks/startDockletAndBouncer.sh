df6fc8ccd575d7d105572d7b120435ca56a51ce9
Ryan Sandor Richards <ryan@runnable.com>
2016-05-02 16:29:02 -0700

    Remove docker folder.

--- docks/startDockletAndBouncer.sh
+++
@@ -1,9 +0,0 @@
-if [[ $# -ne 1 ]]; then
-	echo "usage ./startDockletAndBouncer.sh <NODE_ENV>"
-	echo "where NODE_ENV is one of the following production, staging, integration, development"
-fi
-sudo pm2 kill
-sudo NODE_ENV=$1 pm2 start docklet/bouncer/index.js -n bouncer -i 5
-sudo NODE_ENV=$1 pm2 start docklet/lib/index.js -n docklet
-sudo NODE_ENV=$1 pm2 start docklet/containerGauge/index.js -n containerGauge
-echo "bouncer and docklet started"
