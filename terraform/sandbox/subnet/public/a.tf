b5b02fd50790802dc2f7c1d3f22fa49b9be5f429
Ryan Sandor Richards <ryan@runnable.com>
2016-05-13 11:59:39 -0700

    Tag formatting.

--- terraform/sandbox/subnet/public/a.tf
+++ terraform/sandbox/subnet/public/a.tf
@@ -5,28 +5,30 @@
  * @param {string} provider.region Region for the provider (aws).
  */
 resource "aws_subnet" "public-a" {
-  vpc_id = "${aws_vpc.sandbox.id}"
-  cidr_block = "${var.public-subnet.cidr_block_a}"
-  availability_zone = "${var.provider.region}a"
-  map_public_ip_on_launch = false
   tags {
     Name = "public-subnet-a"
     Environment = "${var.environment}"
   }
+
+  vpc_id = "${aws_vpc.sandbox.id}"
+  cidr_block = "${var.public-subnet.cidr_block_a}"
+  availability_zone = "${var.provider.region}a"
+  map_public_ip_on_launch = false
 }
 
 /**
  * Route table for the sanbox public-a subnet.
  */
 resource "aws_route_table" "public-a" {
+  tags {
+    Name = "public-subnet-a-route-table"
+    Environment = "${var.environment}"
+  }
+
   vpc_id = "${aws_vpc.sandbox.id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.sandbox.id}"
-  }
-  tags {
-    Name = "public-subnet-a-route-table"
-    Environment = "${var.environment}"
   }
 }
 
