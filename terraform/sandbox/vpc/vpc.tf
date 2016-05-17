355ca35ffa5e66e8881adad0111a8eda30ca997f
Ryan Sandor Richards <ryan@runnable.com>
2016-05-16 17:00:47 -0700

    Conform to tag standards.

--- terraform/sandbox/vpc/vpc.tf
+++ terraform/sandbox/vpc/vpc.tf
@@ -2,9 +2,10 @@
  * VPC Resource for the sandbox product infrastructure.
  */
 resource "aws_vpc" "sandbox" {
-  cidr_block = "${var.vpc.cidr_block}"
   tags {
     Name = "vpc"
     Environment = "${var.environment}"
   }
+  
+  cidr_block = "${var.vpc.cidr_block}"
 }
