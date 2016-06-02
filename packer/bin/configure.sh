195bb58af7c3aa2b04ea983780c1fc6c376a0f1a
Christopher M. Neill <chris@runnable.com>
2016-06-02 15:15:59 -0700

    Need to switch branches...

--- packer/bin/configure.sh
+++ packer/bin/configure.sh
@@ -2,13 +2,14 @@
 
 set -x
 
-export PKGS="nginx nmap nodejs rsyslog rsyslog-gnutls htop colordiff screen"
+export PKGS="make unzip openjdk-7-jdk jq nmap htop colordiff"
 
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get autoremove
 sudo apt-get -y install ${PKGS} || echo "Apt installation failure."
 
+curl -O https://releases.hashicorp.com/vault/0.4.1/vault_0.4.1_linux_amd64.zip
 date > ${HOME}/.install.date
 
 exit 0
