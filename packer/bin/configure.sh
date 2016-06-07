1363e75fcd442f5876e33f5854f992b4966c9c16
Christopher M. Neill <chris@runnable.com>
2016-06-07 14:26:22 -0700

    closer

--- packer/bin/configure.sh
+++ packer/bin/configure.sh
@@ -37,7 +37,7 @@ rm -f vault*
 # weave
 curl --silent -q -O https://github.com/weaveworks/weave/releases/download/v1.4.6/weave
 sudo install -c -m 755 weave /usr/local/bin || echo "Could not install weave."
-rm -f weaveo
+rm -f weave
 
 # ec2-metadata
 curl --silent -q -O http://s3.amazonaws.com/ec2metadata/ec2-metadata
