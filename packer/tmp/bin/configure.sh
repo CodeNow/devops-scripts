3ba9ce19ff216eaeddecbfb750aff89716dc7437
Christopher M. Neill <chris@runnable.com>
2016-07-04 17:15:57 -0700

    temp mods to configure.sh and eru_intercom_id

--- packer/tmp/bin/configure.sh
+++ packer/tmp/bin/configure.sh
@@ -9,16 +9,16 @@ APT_OPTS="""-q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--for
 sudo mkdir /docker
 sudo chmod 755 /docker
 sudo chown root.root /docker
-sed 's/\/mnt/\/docker/g' /etc/fstab > /tmp/fstab
-sudo umount /mnt
-sudo mv /tmp/fstab /etc/fstab
-sudo mount /docker
+#sed 's/\/mnt/\/docker/g' /etc/fstab > /tmp/fstab
+#sudo umount /mnt
+#sudo mv /tmp/fstab /etc/fstab
+#sudo mount /docker
 
 # Docker Init
 sudo DEBIAN_FRONTEND=noninteractive apt-get ${APT_OPTS} install apt-transport-https ca-certificates
 sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list.d/docker.list
-printf "Package: docker-engine\nPin: version 1.11*\mPin-Priority: 1001\n" | sudo tee -a /etc/apt/preferences.d/docker-engine 
+printf "Package: docker-engine\nPin: version 1.10.2*\nPin-Priority: 1001\n" | sudo tee -a /etc/apt/preferences.d/docker-engine 
 
 # NodeJS Init
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
