68a985d646cc7b723a52c6c0d74c0e899b741afc
Anandkumar Patel <anand@runnable.com>
2016-07-20 14:46:49 -0700

    add nginx and reggi to gamma

--- ansible/roles/container_kill_start/files/findTagRunning.sh
+++ ansible/roles/container_kill_start/files/findTagRunning.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 IMAGE_NAME="$1"
-CONTAINERS=`docker ps | grep -v '^CONTAINER' | awk '{print $1}'`
+CONTAINERS=`docker ps -a | grep -v '^CONTAINER' | awk '{print $1}'`
 
 if [ "" = "${CONTAINERS}" ] ; then
     exit 0
 else
     for container in ${CONTAINERS} ; do
-        docker inspect "${container}" | grep -q '"Image": "'"${IMAGE_NAME}":
+        docker inspect "${container}" 2>/dev/null| grep -q '"Image": "'"${IMAGE_NAME}": > /dev/null 2>&1
         if [ ${?} -eq 0 ] ; then
             if [ -z "${RUNNING_CONTAINERS}" ] ; then
                 RUNNING_CONTAINERS="${container}"
@@ -19,5 +19,5 @@ else
 fi
 
 if [ ! -z "${RUNNING_CONTAINERS}" ] ; then
-    echo "${RUNNING_CONTAINERS}"
+    echo "${RUNNING_CONTAINERS//['\t\r\n']}"
 fi
