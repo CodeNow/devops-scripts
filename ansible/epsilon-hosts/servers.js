3b2c8695d38d21e04a0462e086bdc9b3dae8875a
Christopher M. Neill <chris@runnable.com>
2016-08-26 17:26:09 -0700

    use aws.sdk to query for hosts tagged with role

--- ansible/epsilon-hosts/servers.js
+++ ansible/epsilon-hosts/servers.js
@@ -4,76 +4,64 @@
 
 var aws = require('aws-sdk');
 var ec2 = new aws.EC2({
-  accessKeyId: 'AKIAJ3RCYU6FCULAJP2Q',
-  secretAccessKey: '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',
-  region: 'us-west-2'
+    accessKeyId: 'AKIAJ3RCYU6FCULAJP2Q',
+    secretAccessKey: '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',
+    region: 'us-west-2'
 });
 
-var name = process.argv[2];
+var name = process.argv[1];
 
 // console.log(name,"xxx");
 
 var params = {
-  Filters: [
-    // Only search for docks in the cluster security group
+    Filters: [
+        // Only search for docks in the cluster security group
     {
-      Name: 'vpc-id',
-      Values: ['vpc-cdb2a3a8']
-    },
-    // Only fetch instances that are tagged as docks
-    {
-      Name: 'tag:role',
-      Values: [name]
+        Name: 'vpc-id',
+        Values: ['vpc-cdb2a3a8']
     },
     // Only fetch running instances
     {
-      Name: 'instance-state-name',
-      Values: ['running']
+        Name: 'instance-state-name',
+        Values: ['running']
     }
-  ]
+    ]
 };
 
 ec2.describeInstances(params, function (err, data) {
-  if (err) {
-    console.error("An error occurred: ", err);
-    process.exit(1);
-  }
+    if (err) {
+        console.error("An error occurred: ", err);
+        process.exit(1);
+    }
 
-  // Get a set of instances from the describe response
-  var instances = [];
-  data.Reservations.forEach(function (res) {
-    res.Instances.forEach(function (instance) {
-      instances.push(instance);
+    // Get a set of instances from the describe response
+    var instances = [];
+    data.Reservations.forEach(function (res) {
+        res.Instances.forEach(function (instance) {
+            instances.push(instance);
+        });
     });
-  });
 
-  // Map the instances to their private ip addresses
-  // NOTE This will work locally because of the wilcard ssh proxy in the config
-  var hosts = instances.map(function (instance) {
-    return instance.PrivateIpAddress;
-  });
+    // Map the instances to their private ip addresses
+    // NOTE This will work locally because of the wilcard ssh proxy in the config
+    var hosts = instances.map(function (instance) {
+        return instance.PrivateIpAddress;
+    });
 
-  var hostVars = {};
-  instances.forEach(function (instance) {
-    for (var i = 0; i < instance.Tags.length; i++) {
-      if (instance.Tags[i].Key === 'org') {
-        hostVars[instance.PrivateIpAddress] = {
-          host_tags: instance.Tags[i].Value + ',build,run'
-        };
-      }
-    }
-  });
+    var hostVars = {};
+    var out = {}
+    instances.forEach(function (instance) {
+        for (var i = 0; i < instance.Tags.length; i++) {
+            if (instance.Tags[i].Key === 'role' ) {
+                if ( !out[instance.Tags[i].Value]) {
+                    out[instance.Tags[i].Value]={hosts: []}
+                }
+                out[instance.Tags[i].Value].hosts.push( instance.PrivateIpAddress)
+            }
+        }
+    });
 
-  // Output the resulting JSON
-  // NOTE http://docs.ansible.com/ansible/developing_inventory.html
-  console.log(JSON.stringify(
-    {
-      docks: {
-        hosts: hosts
-      },
-     _meta : {
-       hostvars : hostVars
-      }
-    }
-  ));
+    // Output the resulting JSON
+    // NOTE http://docs.ansible.com/ansible/developing_inventory.html
+    console.log(JSON.stringify(out));
 });
