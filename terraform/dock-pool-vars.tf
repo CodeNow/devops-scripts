608f8ebb81fb00759d6467eef18740eaad03cebc
Anandkumar Patel <anand@runnable.com>
2016-10-23 09:51:52 -0700

    fmt

--- terraform/dock-pool-vars.tf
+++ terraform/dock-pool-vars.tf
@@ -1,32 +1,36 @@
 variable "dock_pool_ami_name" {
   type = "map"
+
   default = {
-    delta = "delta-dock-2.0.5"
-    gamma = "gamma-dock-2.0.5"
+    delta   = "delta-dock-2.0.5"
+    gamma   = "gamma-dock-2.0.5"
     epsilon = "epsilon-dock-2.0.5"
   }
 }
 
 variable "dock_pool_lc_name" {
   type = "map"
+
   default = {
-    delta = "delta-lc-dock-pool"
-    gamma = "gamma-lc-dock-pool"
+    delta   = "delta-lc-dock-pool"
+    gamma   = "gamma-lc-dock-pool"
     epsilon = "epsilon-lc-dock-pool"
   }
 }
 
 variable "dock_pool_iam_profile" {
   type = "map"
+
   default = {
-    delta = "delta-dock-pool"
-    gamma = "gamma-dock-pool"
+    delta   = "delta-dock-pool"
+    gamma   = "gamma-dock-pool"
     epsilon = "epsilon-dock-pool"
   }
 }
 
 variable "install_ssm_script" {
   type = "string"
+
   default = <<EOF
   #!/bin/bash
   cd /tmp
@@ -38,27 +42,30 @@ variable "install_ssm_script" {
 
 variable "dock_pool_asg_name" {
   type = "map"
+
   default = {
-    delta = "delta-asg-dock-pool"
-    gamma = "gamma-asg-dock-pool"
+    delta   = "delta-asg-dock-pool"
+    gamma   = "gamma-asg-dock-pool"
     epsilon = "epsilon-asg-dock-pool"
   }
 }
 
 variable "dock_pool_asg_max_size" {
   type = "map"
+
   default = {
-    delta = 10
-    gamma = 10
+    delta   = 10
+    gamma   = 10
     epsilon = 10
   }
 }
 
 variable "dock_pool_asg_min_size" {
   type = "map"
+
   default = {
-    delta = 3
-    gamma = 1
+    delta   = 3
+    gamma   = 1
     epsilon = 1
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,7 @@
 variable "aws_access_key" {}
+
 variable "aws_secret_key" {}
+
 variable "region" {
   default = "us-west-2"
 }
