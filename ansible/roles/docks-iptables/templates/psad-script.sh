21e3ba1101581c92ecbade913cd8e3e99117436c
Anandkumar Patel <anand@runnable.com>
2017-01-09 15:17:05 -0800

    update psad script

--- ansible/roles/docks-iptables/templates/psad-script.sh
+++ ansible/roles/docks-iptables/templates/psad-script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-let alerted_ipaddress=$1
+alerted_ipaddress="${1}"
 echo "looking for container with ip = ${alerted_ipaddress}"
 
 for container_id in $(docker ps -qa); do
-  container_ipaddress="$(docker inspect --format "{{ '{{' }} .NetworkSettings.IPAddress {{ '}}' }}" $container_id)"
+  container_ipaddress="$(docker inspect --format "{{ '{{' }} .NetworkSettings.IPAddress {{ '}}' }}" ${container_id})"
   echo "checking ${container_id}: ${container_ipaddress}"
 
-  if [[ "${container_ipaddress}" -eq "${alerted_ipaddress}" ]]; then
-    let psad_logs=''
+  if [[ "${container_ipaddress}" == "${alerted_ipaddress}" ]]; then
+    psad_logs=""
     psad_logs_files="$(ls /var/log/psad/$alerted_ipaddress/*_email_alert)"
     echo "found container_id, getting logs for ${container_id}: ${container_ipaddress} from ${psad_logs_files}"
 
-    for log_file in "${psad_logs}"; do
-      psad_logs="${psad_logs} \n $(sed '/Whois Information/,$d' ${log_file})"
+    for log_file in "${psad_logs_files}"; do
+      psad_logs="${psad_logs} \n\n $(sed '/Whois Information/,$d' ${log_file})"
     done
 
     curl --header "Content-Type: application/json" \
@@ -21,7 +21,7 @@ for container_id in $(docker ps -qa); do
       --data '{"containerId":"'${container_id}'","logs":"'${psad_logs}'"}' \
       "http://{{ drake_hostname }}/psad"
 
-    echo "killing and removing container_id"
+    echo "killing and removing ${container_id}"
     docker kill "${container_id}"
     docker rm "${container_id}"
   fi
