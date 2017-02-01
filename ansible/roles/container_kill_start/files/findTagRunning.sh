51663ad6f9b4ab998e3cb0bb9691fb6d0639dc82
thejsj <jorge.silva@thejsj.com>
2017-01-31 16:11:59 -0800

    Fix container_kill_start role to also use names

--- ansible/roles/container_kill_start/files/findTagRunning.sh
+++ ansible/roles/container_kill_start/files/findTagRunning.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
 IMAGE_NAME="$1"
+CONTAINER_NAME="$2"
 CONTAINERS=`docker ps -a | grep -v '^CONTAINER' | awk '{print $1}'`
 
 if [ "" = "${CONTAINERS}" ] ; then
+    echo "this is 0"
     exit 0
 else
     for container in ${CONTAINERS} ; do
-        docker inspect "${container}" 2>/dev/null| grep -q '"Image": "'"${IMAGE_NAME}": > /dev/null 2>&1
-        if [ ${?} -eq 0 ] ; then
+        docker inspect "${container}" 2>/dev/null| grep -q '"Image": "'"${IMAGE_NAME}" &> /dev/null
+        HAS_IMAGE=$?
+        if [ ! -z "${CONTAINER_NAME}"  ] ; then
+          docker inspect "${container}" 2>/dev/null| grep -q '"Name": "'"\/${CONTAINER_NAME}" > /dev/null 2>&1
+          HAS_CONTAINER_NAME=$?
+        else
+          HAS_CONTAINER_NAME=0
+        fi
+        if [ ${HAS_IMAGE} -eq 0 ] && [ ${HAS_CONTAINER_NAME} -eq 0 ]; then
             if [ -z "${RUNNING_CONTAINERS}" ] ; then
                 RUNNING_CONTAINERS="${container}"
             else
