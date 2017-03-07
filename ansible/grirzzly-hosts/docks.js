010cf4869941cc0e916ebdad7f0bc8ef572ffe25
thejsj <jorge.silva@thejsj.com>
2017-03-07 15:38:46 -0800

    STEP 1-3: Add ENV, domain, content domain proxy

--- ansible/grirzzly-hosts/docks.js
+++ ansible/grirzzly-hosts/docks.js
@@ -4,9 +4,9 @@
 
 var aws = require('aws-sdk');
 var ec2 = new aws.EC2({
-  accessKeyId: 'AKIAIS2HMUM2REGVTVIQ',
-  secretAccessKey: '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',
-  region: 'us-west-2'
+  accessKeyId: 'AKIAJWTGYN3FCQELH5VQ',
+  secretAccessKey: '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',
+  region: '${AWS_REGION}'
 });
 
 var params = {
@@ -14,7 +14,7 @@ var params = {
     // Only search for docks in the cluster security group
     {
       Name: 'instance.group-id',
-      Values: ['sg-ec0da194'] // This script is the same for all environments
+      Values: ['${AWS_DOCK_SG}'] // This script is the same for all environments
     },
     // Only fetch instances that are tagged as docks
     {
