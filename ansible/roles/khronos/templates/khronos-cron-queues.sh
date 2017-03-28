a33a769dd5aeea8ab3df6d8e1b51af02455cfb75
Anandkumar Patel <anand@runnable.com>
2017-03-28 14:24:33 -0700

    add khronos

--- ansible/roles/khronos/templates/khronos-cron-queues.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --rm {{ container_image }}:{{ container_tag }} bash -c " \
-  for QUEUE in {{ item.cron_queues }}; do \
-    /khronos/bin/cli.js --queue \$QUEUE --job '{}' --host {{ cron_rabbit_host_address }} {{ cron_rabbit_auth }}; \
-  done"
