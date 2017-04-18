58eaa86a1c3542bd5be95416f9cdcac58a60287d
Anandkumar Patel <anand@runnable.com>
2017-04-17 17:45:55 -0700

    fix user

--- ansible/roles/postgres/templates/init-user-db.sh
+++ ansible/roles/postgres/templates/init-user-db.sh
@@ -9,9 +9,9 @@ data:
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
         CREATE DATABASE {{ egret_pg_database }};
-        GRANT ALL PRIVILEGES ON DATABASE {{ egret_pg_database }} TO {{ egret_pg_database }};
+        GRANT ALL PRIVILEGES ON DATABASE {{ egret_pg_database }} TO {{ pg_user }};
         CREATE DATABASE {{ arithmancy_pg_database }};
-        GRANT ALL PRIVILEGES ON DATABASE {{ arithmancy_pg_database }} TO {{ arithmancy_pg_database }};
+        GRANT ALL PRIVILEGES ON DATABASE {{ arithmancy_pg_database }} TO {{ pg_user }};
         CREATE DATABASE {{ big_poppa_pg_database }};
-        GRANT ALL PRIVILEGES ON DATABASE {{ big_poppa_pg_database }} TO {{ big_poppa_pg_database }};
+        GRANT ALL PRIVILEGES ON DATABASE {{ big_poppa_pg_database }} TO {{ pg_user }};
     EOSQL
